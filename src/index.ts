569a21f44a270686644c6717b35ed0bc6eded113
imrany <imranmat254@gmail.com>
2023-05-15 16:07:04 +0300

    removed ejs

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,13 @@ import express from "express"
 import cors from "cors"
 import * as dotenv from "dotenv"
 import router from "./routes/api"
-import views from "./routes/view"
 dotenv.config()
 
 const app=express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.set('view engine','ejs');
 app.use(cors({}))
 app.use("/api",router)
-app.use('/admin',views)
 
 const port=process.env.PORT||8000
 app.listen(port,()=>{
