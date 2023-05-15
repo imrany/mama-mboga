ad3a6fcdb28bbcbf3220eff7b40464f87b4bba6e
imrany <imranmat254@gmail.com>
2023-05-15 16:09:53 +0300

    added cors options and changed the server port to 8080

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,17 @@ import * as dotenv from "dotenv"
 import router from "./routes/api"
 dotenv.config()
 
+const corsOptions={
+    origin:["http://localhost:3000","127.0.0.1:3000"]
+}
+
 const app=express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(cors({}))
+app.use(cors(corsOptions))
 app.use("/api",router)
 
-const port=process.env.PORT||8000
+const port=process.env.PORT||8080
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`)
 })
