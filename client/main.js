a50080c9cdb2f2258a94eba2760060acb9b0e2d3
imrany <imranmat254@gmail.com>
2023-05-16 12:44:07 +0300

    setting up project

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/favicon.png'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
