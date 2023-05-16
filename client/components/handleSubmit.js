cd0958a4e4058c2cb78fe718eeb3a42c57d6a0de
imrany <imranmat254@gmail.com>
2023-05-16 21:24:37 +0300

    worked on the create account form

--- client/components/handleSubmit.js
+++ client/components/handleSubmit.js
@@ -23,6 +23,39 @@ const handleLoginSubmit=(element)=>{
     })
 }
 
+const handleCreateAccount=(element)=>{
+    element.addEventListener("submit",async(e)=>{
+        e.preventDefault()
+        try {
+            const phone_number=e.target.phone.value
+            const password=e.target.password.value
+            const confirm_password=e.target.confirm_password.value
+            const username=e.target.username.value
+            if(password===confirm_password){
+                const url=`http://localhost:3000/api/auth/create_account`
+                const response=await fetch(url,{
+                    method:"POST",
+                    headers:{
+                        "content-type":"application/json"
+                    },
+                    body:JSON.stringify({
+                        phone_number,
+                        confirm_password,
+                        username
+                    })
+                })
+                const parseRes=await response.json()
+                console.log(parseRes)
+            }else{
+                alert("Passwords doesn't match")
+            }
+        } catch (error) {
+            console.log(error.message)
+        }
+    })
+}
+
 export {
     handleLoginSubmit,
+    handleCreateAccount,
 }
--- client/components/signUp.js
+++ client/components/signUp.js
@@ -1,8 +1,10 @@
+import { handleCreateAccount } from "./handleSubmit"
+
 const showSignupPage=(element)=>{
     element.addEventListener("click",()=>{
         document.getElementById("login").innerHTML=`
         <h2>Create new account</h1>    
-        <form class="login-form">
+        <form class="login-form" id="create-account">
             <label>Enter Username</label>
             <input type="text" name="username" placeholder="John Doe" required/>
             <label>Enter Phone number</label>
@@ -10,14 +12,16 @@ const showSignupPage=(element)=>{
             <label>Enter Password</label>
             <input type="password" name="password" required/>
             <label>Confirm Password</label>
-            <input type="password" name="cornfirm-password" required/>
+            <input type="password" name="confirm_password" required/>
             <button>Create account</button>
 
             <a href="./auth.html" style="text-decoration:underline; color:inherit; margin:30px 0; cursor:pointer;">I have an account</a>
         </form>
         `
+        handleCreateAccount(document.getElementById("create-account"))
     })
 }
+
 export{
     showSignupPage
 }
