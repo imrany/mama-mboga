12bb7b8d80cc80adbc573bc3fca00b25e97d4edf
imrany <imranmat254@gmail.com>
2023-05-16 14:13:44 +0300

    going with vanilla css

--- client/main.js
+++ client/main.js
@@ -1,28 +1,10 @@
-import './styles/style.css'
-import './styles/responsive.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/image/logo.png'
-import { setupCounter } from './counter.js'
+import './style.css'
+import mamaLogo from '/image/logo.png'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-    <div class="card">
-      <a href="./pages/auth.html" id="counter" type="button">Login</a>
-    </div>
+    <img src="${mamaLogo}" class="logo" alt="Vite logo" />
+    <h1>Mama mboga!</h1>
+    <a href="./pages/auth.html" id="counter" type="button">Login</a>
   </div>
 `
-
-setupCounter(document.querySelector('#counter'))
