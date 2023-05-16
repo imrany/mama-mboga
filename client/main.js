46c97818b37236711f574d6ba2a73f1c32e813d4
imrany <imranmat254@gmail.com>
2023-05-16 15:38:02 +0300

    added grid

--- client/main.js
+++ client/main.js
@@ -1,6 +1,41 @@
 import './styles/style.css'
 import './styles/responsive.css'
 import mamaLogo from '/image/logo.png'
+import cabbage from '/image/cabbage.png'
+import { handleSearch } from './components/handleSearch'
+
+const products=[
+  {
+    category:"Greens",
+    product_name:"Sukuma wiki",
+    price:20
+  },
+  {
+    category:"Greens",
+    product_name:"Spinach",
+    price:30
+  },
+  {
+    category:"Greens",
+    product_name:"Cabbage",
+    price:20
+  },
+  {
+    category:"Legumes",
+    product_name:"Beans",
+    price:60
+  },
+  {
+    category:"Legumes",
+    product_name:"Peas",
+    price:30
+  },
+  {
+    category:"Beverage",
+    product_name:"Coffee",
+    price:40
+  }
+]
 
 document.querySelector('#app').innerHTML = `
   <nav>
@@ -25,9 +60,73 @@ document.querySelector('#app').innerHTML = `
   <div>
 
   <div style="display:flex;">
-    <input class="search-input" type="search" placeholder="Search for a product..."/>
+    <input class="search-input" name="Search" type="search" placeholder="Search for a product..."/>
     <i class="fa fa-lock" style="font-size:40px; margin:0 20px;"></i>
   </div>
     <h2>Welcome to Mama mboga!</h2>
+
+    <div class="products-window">
+    </div>
   </div>
 `
+
+document.querySelector(".products-window").innerHTML=`
+<div class="products">
+  <div>
+    <img class="image" src="${cabbage}" alt="cabbage"/>
+    <p>Selling Cabbage @ ksh20.00</p>
+    <p>Category <span style="color:green;">Greens</span></p>
+  </div>
+</div>
+<div class="products">
+  <div>
+    <img class="image" src="${cabbage}" alt="cabbage"/>
+    <p>Selling Cabbage @ ksh20.00</p>
+    <p>Category <span style="color:green;">Greens</span></p>
+  </div>
+</div>
+<div class="products">
+  <div>
+    <img class="image" src="${cabbage}" alt="cabbage"/>
+    <p>Selling Cabbage @ ksh20.00</p>
+    <p>Category <span style="color:green;">Greens</span></p>
+  </div>
+</div>
+<div class="products">
+  <div>
+    <img class="image" src="${cabbage}" alt="cabbage"/>
+    <p>Selling Cabbage @ ksh20.00</p>
+    <p>Category <span style="color:green;">Greens</span></p>
+  </div>
+</div>
+
+<div class="products">
+  <div>
+    <img class="image" src="${cabbage}" alt="cabbage"/>
+    <p>Selling Cabbage @ ksh20.00</p>
+    <p>Category <span style="color:green;">Greens</span></p>
+  </div>
+</div>
+<div class="products">
+  <div>
+    <img class="image" src="${cabbage}" alt="cabbage"/>
+    <p>Selling Cabbage @ ksh20.00</p>
+    <p>Category <span style="color:green;">Greens</span></p>
+  </div>
+</div>
+<div class="products">
+  <div>
+    <img class="image" src="${cabbage}" alt="cabbage"/>
+    <p>Selling Cabbage @ ksh20.00</p>
+    <p>Category <span style="color:green;">Greens</span></p>
+  </div>
+</div>
+<div class="products">
+  <div>
+    <img class="image" src="${cabbage}" alt="cabbage"/>
+    <p>Selling Cabbage @ ksh20.00</p>
+    <p>Category <span style="color:green;">Greens</span></p>
+  </div>
+</div>
+`
+handleSearch(document.querySelector(".search-input"))
