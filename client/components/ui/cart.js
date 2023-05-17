2e69f9349a308ffbfa81da8768c9da0afaaa76b9
imrany <imranmat254@gmail.com>
2023-05-17 13:02:08 +0300

    fixing add to cart rsponsive css

--- client/components/ui/cart.js
+++ client/components/ui/cart.js
@@ -5,8 +5,8 @@ const showCart=(element,index,items)=>{
                 document.querySelector(".cart").innerHTML=`
                 <div class="item">
                     <div>
-                        <p style="font-size:40px; font-weight:600; margin-bottom:-20px;">Add to Cart</p>
-                        <i class="fa fa-close close-cart" style="cursor:pointer; font-size:30px; float:right; margin:-40px 30px 0 0 ;"></i>
+                        <p class="cart-header">Add to Cart</p>
+                        <i class="fa fa-close close-cart"></i>
                     </div>
                         <img src="${i.image_url}" class="image" alt="image"/>
                         <p style="font-size:20px;">Selling ${i.product_name} @ Ksh ${i.price}.00</p>
