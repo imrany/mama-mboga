502db5a435b0f586c703b5ca917d7e1ec0565194
imrany <imranmat254@gmail.com>
2023-05-17 15:01:32 +0300

    added more items

--- client/data.js
+++ client/data.js
@@ -2,16 +2,31 @@
 import cabbage from '/image/cabbage.png'
 import apple from '/image/apple-removebg-preview_360.png'
 import mango from '/image/Mango-removebg-preview.png'
+import spinach from "/image/spi.jpeg"
+import sukuma from "/image/sk.jpg"
+import passion from "/image/passion.jpeg"
+import onion from "/image/on.jpg"
+import pawpaw from "/image/pawpaw.jpg"
+import pine from "/image/pine.jpeg"
+import tomato from "/image/twotomatoes.jpg"
+import ho from "/image/ho.jpeg"
+import grapes from "/image/grapes.jpg"
+import chilly from "/image/chi.jpg"
+import avocado from "/image/avocado.jpg"
+import banana from '/image/bana.jpeg'
+import Brocolli from "/image/broc.jpg"
+import carrot from "/image/carrots.jpg"
+import calard from "/image/colard.jpeg"
 
 const products=[
     {
-      image_url:cabbage,
+      image_url:sukuma,
       category:"Vegetables",
       product_name:"Sukuma wiki",
       price:20
     },
     {
-      image_url:cabbage,
+      image_url:spinach,
       category:"Vegetables",
       product_name:"Spinach",
       price:30
@@ -23,10 +38,82 @@ const products=[
       price:20
     },
     {
+      image_url:onion,
+      category:"Vegetables",
+      product_name:"Onion",
+      price:20
+    },
+    {
+      image_url:tomato,
+      category:"Vegetables",
+      product_name:"Tomato",
+      price:20
+    },
+    {
+      image_url:chilly,
+      category:"Vegetables",
+      product_name:"Chilly",
+      price:20
+    },
+    {
+      image_url:avocado,
+      category:"Fruits",
+      product_name:"Avocado",
+      price:20
+    },
+    {
+      image_url:carrot,
+      category:"Vegetables",
+      product_name:"Carrots",
+      price:20
+    },
+    {
+      image_url:calard,
+      category:"Vegetables",
+      product_name:"Colard",
+      price:20
+    },
+    {
+      image_url:ho,
+      category:"Vegetables",
+      product_name:"Ho Ho",
+      price:20
+    },
+    {
+      image_url:grapes,
+      category:"Fruits",
+      product_name:"Grapes",
+      price:50
+    },
+    {
+      image_url:pawpaw,
+      category:"Fruits",
+      product_name:"Pawpaw",
+      price:20
+    },
+    {
+      image_url:pine,
+      category:"Fruits",
+      product_name:"Pineapple",
+      price:20
+    },
+    {
         image_url:apple,
         category:"Fruits",
         product_name:"Apple",
         price:50
+    },
+    {
+      image_url:banana,
+      category:"Fruits",
+      product_name:"Banana",
+      price:20
+    },
+    {
+      image_url:Brocolli,
+      category:"Vegetables",
+      product_name:"Brocolli",
+      price:20
     },
     {
         image_url:mango,
@@ -35,7 +122,7 @@ const products=[
         price:50
     },
     {
-      image_url:mango,
+      image_url:passion,
       category:"Fruits",
       product_name:"Passion",
       price:30
