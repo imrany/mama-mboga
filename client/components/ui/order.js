2b6b11f463e64f88b91d0f58e66119cd8320fe4c
imrany <imranmat254@gmail.com>
2023-05-18 10:42:27 +0300

    worked on order page and its UI

--- client/components/ui/order.js
+++ client/components/ui/order.js
@@ -17,27 +17,36 @@ const ordersArray=(element,orders)=>{
 
 const viewOrder=(element)=>{
     element.addEventListener("click",()=>{
-    const transaction=db.transaction("Orders","readwrite")
-    const orderStore=transaction.objectStore("Orders")
-    const getItems=orderStore.getAll()
-    getItems.onsuccess = (ev) => {
-        console.log(getItems.result);
-        let li=`
-        <h1>order</h1>
-        `
-        document.getElementById("app").innerHTML=`
-        <div>
-            <h2>Orders</h2>
-            <div class="order-list">
+        const transaction=db.transaction("Orders","readwrite")
+        const orderStore=transaction.objectStore("Orders")
+        const getItems=orderStore.getAll()
+        getItems.onsuccess = (ev) => {
+            document.getElementById("app").innerHTML=`
+            <div>
+                <h2>Orders</h2>
+                <div class="order-list">
+                </div>
             </div>
-        </div>
-        `
-        document.querySelector(".order-list").innerHTML+=li
-    };
-    getItems.onerror = (err) => {
-        console.log('error in request to add');
-        window.location.href="/index.html"
-    };
+            `
+            console.log(getItems.result);
+            const order_items=getItems.result
+            order_items.forEach(i => {
+                let li=`
+                <div key=${i.id} class="products">
+                    <div>
+                        <img class="image" src="${i.image_url}" alt="${i.product_name}"/>
+                        <p>Selling ${i.product_name} @ ksh${i.price}.00</p>
+                        <p>Category  ${i.category}</p>
+                    </div>
+                </div>
+                `
+                document.querySelector(".order-list").innerHTML+=li
+            });
+        };
+        getItems.onerror = (err) => {
+            console.log('error in request to add');
+            window.location.reload()
+        };
     })
 }
 
